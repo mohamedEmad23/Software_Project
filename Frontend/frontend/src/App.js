284f7b7138789f38123726e0829755cecbc45518
Emad <mohammed.gad@student.giu-uni.de>
2024-05-13 20:44:23 +0300

    Initial commit

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const title = 'Welcome to the new Blog'
+  return (
+    <div className="App">
+     <div className="content">
+        <h1>{ title }</h1>
+        
+     </div>
+    </div>
+  );
+}
+
+export default App;
