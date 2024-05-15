d43707fdf31243c7ce997b5f978cca23b346f9b4
Emad <mohammed.gad@student.giu-uni.de>
2024-05-15 12:00:31 +0300

    Added new snippets and code, check the server.js backend

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <RegisterPage />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/register" element={<RegisterPage />} />
+                {/* Add more routes as needed */}
+            </Routes>
+        </Router>
     );
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
+const https = require('https');
+const fs = require('fs');
 const express=require('express');
 const connectDB = require('./config/db');
 const userRoute = require('./routes/userRoute');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const userControllers = require('./controllers/userControllers.js');
-
+const cors = require('cors');
 const adminRoute = require('./routes/adminRoute');
 const authenticationMiddleware=require("./middleware/authenticationMiddleware")
 const orderRoute = require('./routes/orderRoute');
 
 const app = express();
+
+//New code
+app.use(cors({
+    origin: 'https://localhost:3000', // replace with the origin of your frontend
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -24,10 +32,18 @@ app.use(authenticationMiddleware)
 app.use("/api/v1", adminRoute);
 app.use("/api/v1", orderRoute);
 
+ const options = {
+     key: fs.readFileSync('C:\\Users\\M-EMAD\\WebstormProjects\\Recent_Test_Sample\\Frontend\\frontend\\key.pem'),
+     cert: fs.readFileSync('C:\\Users\\M-EMAD\\WebstormProjects\\Recent_Test_Sample\\Frontend\\frontend\\cert.pem')
+ };
+
+ https.createServer(options, app).listen(5000, () => {
+     console.log('HTTPS server running on port 5000');
+ });
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+ // app.listen(port, () => {
+ //     console.log(`Server is running on port ${port}`);
+ // });
 
 
